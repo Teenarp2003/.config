e1747f1d398fd63d0d5d410d192bf32118466a0d
Praneet Guha Roy <praneet.guharoy@gmail.com>
2022-10-31 20:11:27 +0530

    .config/fish,.config/polybar, .config/sxhkd
    Added Functions to polybar, sxhkd, fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
-#   neofetch
 # Commands to run in interactive sessions can go here
 function fish_greeting
    echo 
@@ -8,13 +7,23 @@ function fish_greeting
    echo ░▒█▒░█▄▄░█▄▄░█▒▀█░█▀█░█▀▄░█▀▒░▀▀░█▄░█▄█░█▄░██
    echo Time: (set_color yellow; date +%T; set_color normal)
    echo Machine: $hostname
-   echo Shell: Fish
+   echo Shell: $SHELL
 end
 end
+
 starship init fish | source
+
+alias 'connect'='nmcli --ask con up JioFiber-AP_5G'
+alias ls='exa -lah --icons'
+alias vim='nvim'
+alias vi='nvim'
+alias edit='nvim'
+alias cls='clear'
+
+if test -f /home/teenarp2003/.autojump/share/autojump/autojump.fish; . /home/teenarp2003/.autojump/share/autojump/autojump.fish; 
+end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/teenarp2003/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
