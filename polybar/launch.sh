97cf27036ac3f7416ff7819602c8641072fce100
Praneet Guha Roy <praneet.guharoy@gmail.com>
2022-11-25 23:24:24 +0530

    .config: Add Dunst and Lxpolkit Support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,11 @@
-
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
-killall -q polybar
-
+# Terminate already running bar instances and env programs
+killall -q polybar nitrogen dunst lxpolkit glava 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
+# Launch picom
+picom --animations --detect-rounded-corners --experimental-backends -b
 # Launch
 polybar example &
-nitrogen --set-auto --random
 echo "Bar launched..."
